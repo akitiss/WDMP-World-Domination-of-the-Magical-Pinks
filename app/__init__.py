6e1e75c7a9bc04145bc4e7f42432a187a729ca0f
Joshua Liu <jliu31@stuy.edu>
2022-12-07 18:09:51 -0500

    Made the and home slightly better

--- app/__init__.py
+++ app/__init__.py
@@ -5,20 +5,29 @@ WDMP: . . .
 from flask import Flask, session, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-#HARD user and pass
+app.secret_key = "tmep"
+# user and pass
 user = "user"
 passwd = "pass"
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if ( request.method == "GET" ):
-        return render_template("login.html")
-    if ( request.form.get("username") == user and request.form.get("password") == passwd ):
-        return redirect(url_for("home_page"))
+        return render_template("login.html") # This is for accessing the page
+    if ( request.form.get("username") == user ):
+        if( request.form.get("password") == passwd ):
+            session["USERNAME"] = request.form.get("username")
+            return redirect(url_for("home_page"))
+        return render_template("login.html", response="The Username and Password do not match")
+    return render_template("login.html", response="Please Enter a Valid Username")
 
-@app.route("/Home", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def home_page():
-    return "heLLO"
+    if(session.get("USERNAME", None) == None):
+        stat = "Please Login"
+    else:
+        stat = F"Logged in as {session['USERNAME']}"
+    return render_template("index.html", status=stat)
 
 if __name__ == "__main__":
     app.debug = True

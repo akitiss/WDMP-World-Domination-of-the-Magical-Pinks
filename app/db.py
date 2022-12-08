e0bd76e6887ecedbbafb2f495e4ee85c8bad85e8
Joshua Liu <jliu31@stuy.edu>
2022-12-07 19:35:06 -0500

    Added db with one function

--- app/db.py
+++ app/db.py
@@ -1,2 +1,22 @@
 import sqlite3
 
+DB_FILE = "test.db"
+
+db = sqlite3.connect(DB_FILE)
+c = db.cursor() # Create the three tables if they dont exist yet
+c.executescript(""" 
+    create TABLE if NOT EXISTS user(u_id int primary key, username varchar(20), password varchar(30));
+    create TABLE if NOT EXISTS savedtrips(u_id int, trip_id int, PRIMARY KEY (u_id, trip_id));
+    create TABLE if NOt EXISTS tripinfo(trip_id primary key, country text, city text, hotel text);
+""")
+c.close()
+
+def get_username(id):
+    c = db.cursor()
+    c.execute("select username FROM user WHERE u_id = ?", (id,))
+    result = c.fetchone()
+    if(len(result) == 0):
+        return None
+    else:
+        return result[0]
+    

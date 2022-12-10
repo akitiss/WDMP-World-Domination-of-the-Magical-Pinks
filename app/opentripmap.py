1e716a68f6877fc3c08f9d5fb0d4766eed3b1103
Aahan Mehta <aahanmehta@Aahans-MBP.lan>
2022-12-09 22:03:13 -0500

    somewhat productive session, team trippin ftw

--- app/opentripmap.py
+++ app/opentripmap.py
@@ -17,19 +17,47 @@ def get_places(city, radius, kind):
     places_info = f"https://api.opentripmap.com/0.1/en/places/radius?radius={radius}&lon={coords['lon']}&lat={coords['lat']}&kinds={kind}&format=GeoJSON&limit={limit}&apikey={key}"
     places_json = requests.get(places_info)
     places = places_json.json()
-    # print(places)
-    places_dict = {}
-    for dic in places:
-        print(dic)
-        name = dic['name']
-        places_dict[name] = ( dic['wikipedia_extracts']['text'], dic['image'] , dic['url'])
+    places_list =[]
+    for dictionary in places:
+        places_list.append( (dictionary['name'], dictionary['xid']) )
+    # places_dict = {}
 
+    master_place_details = {}
+    for place in places_list:
+        place_details_url = f"https://api.opentripmap.com/0.1/en/places/xid/{place[1]}?apikey={key}"
+        place_json = requests.get(place_details_url)
+        place_dict = place_json.json() #very specific for one place
+        
+        
+        lat,lon = place_dict['point']['lat'], place_dict['point']['lon']
+        deets= {'xid': place[1], 'lat': lat, 'lon': lon}
+        if 'url' in place_dict:
+            url = place_dict['url']
+            deets['url'] = url
+        else:
+            url = place_dict['otm']
+            deets['url'] = url
+        
+        if 'image' in place_dict:
+            img = place_dict['image']
+            deets['img'] = img
 
-    print(places_dict) #returns arr of genre of places in city, using radius in an arr with max len limit
+        if 'wikipedia_extracts' in place_dict:
+            wiki_extract = place_dict['wikipedia_extracts']['text']
+            deets['wiki_extract'] = wiki_extract
+        
 
+        master_place_details[place[0]] = deets
+
+    # print(master_place_details)
+    # print(places_list)
+    return master_place_details
 
 def get_hotels(city):
-    get_places(city, 5000, "other_hotels")
+    return get_places(city, 5000, "other_hotels")
 
+def get_naughty(city):
+    return get_places(city, 5000, "adult")
 
-get_hotels('Paris')
+x = get_naughty('Paris')
+print(x)
